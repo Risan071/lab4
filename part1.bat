a266a73286cb1ddd49a01d0700c7e6d0bc22b996
Risan071 <sigvladik@gmail.com>
2024-05-15 20:45:45 +0300

    Add files via upload

---
+++ part1.bat
@@ -0,0 +1,6 @@
+@echo off
+
+xcopy F:\Laboratorni\PZ-23-3\SigaevVladislav\batch\OpenFolder\copyhelp.txt F:\Laboratorni\PZ-23-3\SigaevVladislav\batch\HiddenFolder
+ren F:\Laboratorni\PZ-23-3\SigaevVladislav\batch\HiddenFolder\copyhelp.txt copied_copyhelp.txt
+pause
+
---
+++ part2.bat
@@ -0,0 +1,3 @@
+@echo off
+dir F:\Laboratorni\PZ-23-3 /-s
+pause
